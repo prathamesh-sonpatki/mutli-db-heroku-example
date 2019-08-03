2284e559cbec6e2bb59ade09bb94e972d0c8a15f
Prathamesh Sonpatki <csonpatki@gmail.com>
2019-08-03 22:32:40 +0530

    Add connects_to info to application_record

--- app/models/animal_base.rb
+++ app/models/animal_base.rb
@@ -1,5 +1,5 @@
-class AnimalBase < ActiveRecord::Base
+class AnimalBase < ApplicationRecord
   self.abstract_class = true
 
-  connects_to database: { writing: :animals }
+  connects_to database: { writing: :animals, reading: :animals }
 end
