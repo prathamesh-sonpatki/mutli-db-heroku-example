a8752cbb16cdc1ad09f8fff5cf2aa4eefe2318b4
Prathamesh Sonpatki <csonpatki@gmail.com>
2019-08-04 18:23:54 +0530

    New model for animals db and change of order of databases

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_03_162345) do
+ActiveRecord::Schema.define(version: 2019_08_04_125122) do
+
+  create_table "authors", force: :cascade do |t|
+    t.string "email"
+    t.string "name"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
